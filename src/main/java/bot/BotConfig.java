66f68fb441d8afd8f145be99c98cf7d9e9cb93ed
Yug Dassani <yugdassani@Yugs-MacBook-Air.local>
2017-07-18 15:07:22 +0530

    First

---
+++ src/main/java/bot/BotConfig.java
@@ -0,0 +1,13 @@
+package bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotConfig {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotConfig.class, args);
+	}
+
+}
---
+++ src/main/java/bot/BotController.java
@@ -0,0 +1,22 @@
+package bot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/webhook")
+public class BotController {
+
+    @RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
+
+        System.out.println(obj);
+
+
+        return new WebhookResponse("Hello!", "https://www.google.com");
+    }
+}
