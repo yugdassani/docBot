82d02f8a1a6dd57d950307ec5985465368288d78
yugdassani <ydassani@gmail.com>
2017-07-24 15:45:03 +0530

    Update BotController.java

--- src/main/java/bot/BotController.java
+++ src/main/java/bot/BotController.java
@@ -13,10 +13,22 @@ public class BotController {
 
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
-
-        System.out.println(obj);
-
-
-        return new WebhookResponse(obj, "https://www.google.com");
+  System.out.println(obj);
+        String indate = obj.substring(obj.indexOf("inDate"),20);
+        SimpleDateFormat f = new SimpleDateFormat("yyyyMMdd");
+        Date date = new Date();
+        String d = "20170724";
+        Date date1 = new Date();
+        try {
+            date = f.parse(indate);
+           date1 = f.parse(d);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        if (date.after(date1))
+            return new WebhookResponse("https://www.google.com", "https://www.google.com");
+        else
+            return new WebhookResponse("hey","hey");
     }
 }
