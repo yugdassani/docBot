752740509368bf7f8d4aa03f473c26a4529fc642
yugdassani <ydassani@gmail.com>
2017-07-24 16:35:16 +0530

    Update BotController.java

--- src/main/java/bot/BotController.java
+++ src/main/java/bot/BotController.java
@@ -18,7 +18,7 @@ public class BotController {
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
   System.out.println(obj);
-        String indate = obj.substring(obj.indexOf("inDate")+10,obj.indexOf("inDate")+20);
+        String indate = obj.substring(obj.indexOf("inDate")+9,obj.indexOf("inDate")+19);
         SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
         Date date1 =new Date();
