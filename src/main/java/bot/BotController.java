0821b3d13aedbba039d2286525f55a10468d6ffe
yugdassani <ydassani@gmail.com>
2017-07-24 16:20:54 +0530

    Update BotController.java

--- src/main/java/bot/BotController.java
+++ src/main/java/bot/BotController.java
@@ -33,6 +33,6 @@ public class BotController {
         if (date.after(date1))
             return new WebhookResponse(date1.toString(), "https://www.google.com");
         else
-           return new WebhookResponse(obj,"hey");
+           return new WebhookResponse(date1.toString() + date.toString(),"hey");
     }
 }
