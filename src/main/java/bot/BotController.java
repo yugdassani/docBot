7deeb19e40a11c34e83499f3ea632a07d92bb28e
yugdassani <ydassani@gmail.com>
2017-08-02 15:09:44 +0530

    Update BotController.java

--- src/main/java/bot/BotController.java
+++ src/main/java/bot/BotController.java
@@ -36,15 +36,16 @@ public class BotController {
       	String files = null;
         
         String[] docInfo = obj.substring(obj.indexOf("all_docs")+12,obj.indexOf("}",obj.indexOf("all_docs")+12)).split(":");
-	    
+	
+	String fileName = docInfo[1].replace("\"","");
 	
        
-        /*files = getDocumentURL("docs",docInfo);
+        files = getDocumentURL("docs",fileName);
           
 	if(files != null)
        	    return new WebhookResponse(files,"text");
-	else*/
-	    return new WebhookResponse(docInfo[1],null);
+	else
+	    return new WebhookResponse(fileName,null);
     }
     
   public static String getDocumentURL(String category, String documentName) {
